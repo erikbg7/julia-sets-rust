8d8897f9c7dabc51911f5846a50d42bb526c1924
erikbg7 <erik.bg7@gmail.com>
2022-03-21 13:17:16 +0100

    Add modal and time toast

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,32 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import SettingsSidebar from '../components/SettingsSidebar';
-import { Box } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Text,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  NumberInput,
+  NumberInputField,
+  useDisclosure,
+  useToast,
+} from '@chakra-ui/react';
 import { JuliaHeader } from '../components/JuliaHeader';
+import { Function } from '../components/Function';
 import { JuliaImage } from '../components/JuliaImage';
+import { FunctionInput } from '../components/FunctionInput';
 
 const Home = () => {
   const [size, setSize] = useState(1);
   const [color, setColor] = useState('grayScale');
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
 
   const onGenerateJuliaSet = () => {
     const canvas = document.getElementById('canvas');
@@ -19,17 +39,44 @@ const Home = () => {
       console.warn(canvas.width, canvas.height, baseImage, baseImage.height, baseImage.width);
       //@ts-ignore
       context.drawImage(baseImage, 0, 0, baseImage.width, baseImage.height, 0, 0, canvas.width, canvas.height);
+      toast({
+        title: 'Finished in 117.6 ms',
+        status: 'info',
+        variant: 'solid',
+        isClosable: true,
+        position: 'top-right',
+      });
     }, 300);
     // context.drawImage(baseImage, 0, 0, 600, 600, 0, 0, 360, 360);
   };
 
+  const onNewFunction = useCallback(() => {
+    onClose();
+    onGenerateJuliaSet();
+  }, [onClose, onGenerateJuliaSet]);
+
   return (
     <Box height={'100vh'} display={['block', 'grid']} gridTemplateColumns={'320px 1fr'}>
       <JuliaHeader display={['flex', 'none']} />
-      <SettingsSidebar onSizeChange={setSize} onColorChange={setColor} onGenerateJuliaSet={onGenerateJuliaSet} />
+      <SettingsSidebar onSizeChange={setSize} onColorChange={setColor} onGenerateJuliaSet={onOpen} />
       <JuliaImage />
-      {/*<Function exp={2} cRe={0.2} cIm={-0.279} />*/}
       {/*{color} kkk {size}*/}
+      <Modal isCentered isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(5px)" />
+        <ModalContent>
+          <ModalHeader>Create a new Julia set</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>We use quadratic polynomials to generate more beautiful sets, those can be expressed as:</Text>
+            <Function exp={2} />
+            <Text>Where c is a complex number. Please, provide the values for the real and imaginary numbers</Text>
+            <FunctionInput />
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onNewFunction}>Generate</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
