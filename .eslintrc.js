4cab5a03e3cd0665e74150097d697ee4d8ebd3af
erikbg7 <erik.bg7@gmail.com>
2022-03-30 17:11:01 +0200

    Fix eslint

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    // turns off the rules which may conflict with prettier
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // force using Logger object
+    'no-console': ['warn'],
+    'no-duplicate-imports': ['warn'],
+
+    // disable explicit return types
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // allow "_" prefixed function arguments
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { args: 'after-used', argsIgnorePattern: '^_', ignoreRestSiblings: true, varsIgnorePattern: '^_+$' },
+    ],
+    '@typescript-eslint/no-empty-function': 'warn',
+    '@typescript-eslint/class-name-casing': 'off',
+    '@typescript-eslint/ban-ts-comment': [
+      'error',
+      {
+        'ts-ignore': 'allow-with-description',
+      },
+    ],
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          object: false,
+        },
+      },
+    ],
+
+    // taken care of by typescript
+    'react/prop-types': 'off',
+    'react/self-closing-comp': [
+      'warn',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    'react/no-unescaped-entities': 'off',
+  },
+};
